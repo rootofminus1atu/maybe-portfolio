91ef5e78d5ac134156d02ec3c764182adb3e90a6
RootOfMinus1ATU <s00240122@atu.ie>
2024-09-28 14:26:17 +0100

    setup react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import Home from './pages/Home.tsx';
+import About from './pages/About.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/maybe-portfolio/",
+    element: <App />,
+    children: [
+      {
+        path: "/maybe-portfolio/",
+        element: <Home />,
+      },
+      {
+        path: "/maybe-portfolio/about",
+        element: <About />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
